3b1445263e2113bf0b8d64aa816eeda7a0d55ab4
Kalle Hallivuori <kato@iki.fi>
2025-09-28 20:56:47 +0300

    fixed moving to ~ correct directions

--- asm/players-move.asm
+++ asm/players-move.asm
@@ -8,64 +8,16 @@ players_move:
     and #$0c                ; 4 = left; 8 = right
     beq @straight_ahead     ; direction did not change
 ; change direction
-    asl                     ; 8 = left; 16 = right
-    sec
-    sbc #$0c                ; -= 12 => -4 = left; 4 = right
+    and #$08
+    beq @turn_left
+    lda #$04 ; turn right
+    jmp @change_direction
+@turn_left:
+    lda #$fc
+@change_direction:
+    clc
     adc plane_direction,x
-    sta plane_direction,x
-; set plane and shadow sprites point to correct direction
-    tay
-    lsr
-    lsr
-    lsr
-    lsr
-    ora #$80
-    sta sprite_pointers, x
-    sta sprite_pointers + 4, x
-    tya
-
-; update dx and dy according to new direction
-; (not accommodating speed just yet)
-    cmp #$80
-    bcc @west; 0-$7f = "East"
-    cmp #$40
-    bcc @south_east
-; "NorthEast"
-    and #$3f
-    tay
-    lda coordinate_of_angle, y
-    sta plane_dy, x
-    eor #$ff
-    sta plane_dx, x
-    jmp @delta_done
-@south_east:
-    cmp #$80
-    bcc @west
-    and #$3f
-    tay
-    lda coordinate_of_angle, y
-    eor #$ff
-    sta plane_dx, x
-    sta plane_dy, x
-    jmp @delta_done
-@west:
-    cmp #$a0
-    bcc @north_west
-    and #$3f
-    tay
-    lda coordinate_of_angle, y
-    sta plane_dx, x
-    eor #$ff
-    sta plane_dy, x
-    jmp @delta_done
-@north_west:
-    and #$3f
-    tay
-    lda coordinate_of_angle, y
-    sta plane_dx, x
-    sta plane_dy, x
-
-@delta_done:
+    jsr set_plane_direction
 
 @straight_ahead:
     clc
@@ -131,4 +83,77 @@ players_move:
     jmp @each_player
 
 @all_done:
+    RTS
+
+set_plane_direction: ; x = plane number 0-3; a = new direction 0 (North) - FF (1 degree West of North)
+    sta plane_direction,x
+; set plane and shadow sprites point to correct direction
+    tay
+    lsr
+    lsr
+    lsr
+    lsr
+    ora #$80
+    sta sprite_pointers, x
+    sta sprite_pointers + 4, x
+    tya
+
+; update dx and dy according to new direction
+; (not accommodating speed just yet)
+    cmp #$80
+    bcs @west; 0-$7f = "East"
+    cmp #$40
+    bcs @south_east; 0-3f = "NorthEast"
+; "NorthEast"
+    tay
+    lda coordinate_of_angle, y
+    sta plane_dx, x ; as is: the more off of north, the faster we go east
+    tya
+    eor #$3f ; index of coordinate of opposite angle
+    tay
+    lda coordinate_of_angle, y
+    eor #$ff ; negative opposite: we go north the slower the faster we go east
+    sta plane_dy, x
+    RTS
+
+@south_east: ; 40-7f
+    and #$3f
+    tay
+    lda coordinate_of_angle, y
+    sta plane_dy, x ; as is: the more toward south, the faster we go south
+    tya
+    eor #$3f ; index of coordinate of opposite angle
+    tay
+    lda coordinate_of_angle, y
+    sta plane_dx, x
+    RTS
+
+@west:
+    cmp #$c0
+    bcs @north_west
+; 80-9f = "SouthWest"
+    and #$3f
+    tay
+    lda coordinate_of_angle, y
+    eor #$ff ; negative: go west
+    sta plane_dx, x
+    tya
+    eor #$3f ; index of coordinate of opposite angle
+    tay
+    lda coordinate_of_angle, y
+    sta plane_dy, x
+    RTS
+
+@north_west: ; a0-ff
+    and #$3f
+    tay
+    lda coordinate_of_angle, y
+    eor #$80 ; negative opposite: the more toward north, the slower we go west
+    sta plane_dx, x
+    tya
+    eor #$3f ; index of coordinate of opposite angle
+    tay
+    lda coordinate_of_angle, y
+    eor #$80 ; negative opposite: the more toward north, the faster we go north
+    sta plane_dy, x
     RTS
