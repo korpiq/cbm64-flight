b2aa3c2bd404e99adc9255f24dfd5a18beb6c282
Kalle Hallivuori <kato@iki.fi>
2026-02-25 21:08:21 +0200

    added charset with clouds

--- graphics/all.asm
+++ graphics/all.asm
@@ -1,4 +1,13 @@
-; .align $40 – once we load directly into correct memory area, bank#1, ~$4000
+; .align $40 ; once we load directly into correct memory area, bank#1, ~$4000
+; .byte 0
+sprites_balls:
 .include "balls.asm"
+.out .sprintf("sprites_balls = %d - %d", sprites_balls, *)
 plane_sprites:
 .incbin "plane-sprites.bin"
+.out .sprintf("plane_sprites = %d - %d", plane_sprites, *)
+
+; .byte 0
+charset_map_data:
+.incbin "charset.bin"
+.out .sprintf("charset_map_data = %d - %d", charset_map_data, *)
